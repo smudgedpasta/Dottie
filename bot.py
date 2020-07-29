c857c11019c6171c2739cbaabdd730f931471bc2
smudgedpasta <68189588+smudgedpasta@users.noreply.github.com>
2020-07-29 16:47:55 +0100

    Added event of a member joining or leaving

--- bot.py
+++ bot.py
@@ -6,5 +6,13 @@ dottie = commands.Bot(command_prefix = "d.")
 @dottie.event
 async def on_ready():
     print("I am ready!")
+    
+@dottie.event
+async def on_member_join(member):
+    print(f"{member} has joined the test server.")
+
+@dottie.event
+async def on_member_remove(member):
+    print(f"{member} has left the test server.")
 
 # dottie.run("The token goes here, I'm keeping it hidden due to publicity")
