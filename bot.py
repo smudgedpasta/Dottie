8b133e1f6db0338bbd8f5d66e810130c406f408f
smudgedpasta <68189588+smudgedpasta@users.noreply.github.com>
2020-07-29 19:19:41 +0100

    Added some simple commands

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 
+import random
+
 dottie = commands.Bot(command_prefix = "d.")
 
 @dottie.event
@@ -15,4 +17,23 @@ async def on_member_join(member):
 async def on_member_remove(member):
     print(f"{member} has left the test server.")
 
+@dottie.command(aliases = ["Hi", "Hello", "hi", "HI", "HELLO", "hElLo", "HeLlO", "hI"])
+async def hello(ctx):
+    await ctx.send("Hello! 👋")
+
+@dottie.command()
+async def ping(ctx):
+    await ctx.send(f"Ping! I pong back your ping latency was {round(dottie.latency * 1000)}ms.")
+
+@dottie.command(aliases = ["8ball", "AskDottie", "ask"])
+async def _8ball(ctx, *, question):
+    responses = ["Heck yeah!",
+                 "Hmm, maybe?",
+                 "Probably not...",
+                 "Try it and find out!",
+                 "Heheh, I'd like to see you try.",
+                 "I didn't quite catch that...",
+                 "Ay, ask me later, I'm busy with my 10 hour tunez!"]
+    await ctx.send(f"So you asked... {question}? {random.choice(responses)}")
+    
 # dottie.run("The token goes here, I'm keeping it hidden due to publicity")
