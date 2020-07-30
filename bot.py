8b304a84339ecf1e9723065b6fde2bbdd11fee49
smudgedpasta <68189588+smudgedpasta@users.noreply.github.com>
2020-07-30 10:11:21 +0100

    Bot now logs in a designated channel(s) and tidied code

--- bot.py
+++ bot.py
@@ -8,22 +8,33 @@ with open("./config.json", "r") as f:
     data = json.load(f)
     discord_token = data["token"]
 
-import random
+import asyncio
 
 
-dottie = commands.Bot(command_prefix = "d.")
+dottie = commands.Bot(command_prefix="d.")
+
+
+def print(*args, sep=" ", end="\n"):
+    asyncio.create_task(LOG_CHANNEL.send(str(sep).join(str(i) for i in args) + end))
+    # asyncio.create_task(LOG_CHANNEL_2.send(str(sep).join(str(i) for i in args) + end))
+
 
 @dottie.event
 async def on_ready():
-    print("I am ready!")
-    
+    globals()["LOG_CHANNEL"] = await dottie.fetch_channel(738320254375165962)
+    # globals()["LOG_CHANNEL_2"] = await dottie.fetch_channel(738003426218213389)
+    print("```I am ready!```")
+
+
+import random
+
 @dottie.event
 async def on_member_join(member):
-    print(f"{member} has joined the test server.")
+    print(f"```{member} has joined the test server.```")
 
 @dottie.event
 async def on_member_remove(member):
-    print(f"{member} has left the test server.")
+    print(f"```{member} has left the test server.```")
 
 @dottie.command(aliases = ["Hi", "Hello", "hi", "HI", "HELLO", "hElLo", "HeLlO", "hI"])
 async def hello(ctx):
@@ -43,5 +54,6 @@ async def _8ball(ctx, *, question):
                  "I didn't quite catch that...",
                  "Ay, ask me later, I'm busy with my 10 hour tunez!"]
     await ctx.send(f"So you asked... {question}? {random.choice(responses)}")
-    
+
+
 dottie.run(discord_token)
