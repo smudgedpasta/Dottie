02586b77096d1929b41b31ea9ee8d961653dbd76
smudgedpasta <smudgedpasta@gmail.com>
2020-09-21 20:37:20 +0100

    Moved all FUN commands to cog

--- cogs/owner.py
+++ cogs/owner.py
@@ -2,9 +2,19 @@ import discord
 from discord.ext import tasks, commands
 
 
+def is_owner(ctx):
+    return ctx.message.author.id in [530781444742578188, 201548633244565504]
+
+
 class OWNER(commands.Cog):
     def __init__(self, dottie):
         self.dottie = dottie
+
+    
+    @commands.command(aliases=["check", "cogs"])
+    @commands.check(is_owner)
+    async def cogs_check(self, ctx):
+        await ctx.send("```json\n\"🎉 COGS ARE OPERATIONAL. 🎉\"```")
 
 
 def setup(dottie):
