0bef2aaa87a00d732c3ed2e8b40d6691e4d5e100
Thomas Xin <thomasxin@gmail.com>
2020-12-16 15:50:58 +1100

    of course this needs to work with no arguments too

--- cogs/general.py
+++ cogs/general.py
@@ -154,9 +154,9 @@ class GENERAL(commands.Cog):
 
     @commands.command(aliases=["userinfo", "info", "stats", "userstats"])
     async def profile(self, ctx):
-        query = ctx.message.content.split(None, 1)[-1]
-        if query:
-            member = await self.dottie.find_user(query, guild=ctx.guild)
+        spl = ctx.message.content.split(None, 1)
+        if len(spl) > 1:
+            member = await self.dottie.find_user(spl[-1], guild=ctx.guild)
         else:
             member = ctx.author
         try:
@@ -195,9 +195,9 @@ class GENERAL(commands.Cog):
 
     @commands.command(aliases=["icon"])
     async def avatar(self, ctx):
-        query = ctx.message.content.split(None, 1)[-1]
-        if query:
-            member = await self.dottie.find_user(query, guild=ctx.guild)
+        spl = ctx.message.content.split(None, 1)
+        if len(spl) > 1:
+            member = await self.dottie.find_user(spl[-1], guild=ctx.guild)
         else:
             member = ctx.author
         embed = discord.Embed(colour=member.colour)
