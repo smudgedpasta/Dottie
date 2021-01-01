7df2a3bbaa50d293b8e269be63fcea9ca495da0c
smudgedpasta <smudgedpasta@gmail.com>
2021-01-01 12:53:49 +0000

    Just gonna leave this here lmao

--- cogs/general.py
+++ cogs/general.py
@@ -25,104 +25,104 @@ class GENERAL(commands.Cog):
         self.dottie = dottie
 
 
+#     @commands.command()
+#     async def help(self, ctx):
+#         page1 = discord.Embed(colour=discord.Colour(15277667), timestamp=ctx.message.created_at)
+#         page1.set_author(name="🐾 Help List 🌨️", url="https://github.com/smudgedpasta/Dottie/wiki", icon_url=self.dottie.user.avatar_url_as(format="png", size=4096))
+#         page1.description = "*I think I need heeelp, I'm drowning in myseeelf* 🎵"
+#         page1.set_image(url="https://cdn.discordapp.com/attachments/683233571405561876/746281046231875594/image0.png")
+#         page1.set_footer(icon_url=ctx.author.avatar_url_as(format="png", size=4096), text="Click the reactions to scroll through the pages!")
+
+#         page2 = discord.Embed(colour=discord.Colour(15277667))
+#         page2.description = """*I think I need heeelp, I'm drowning in myseeelf* 🎵\n
+# **:crossed_swords: __MODERATION__ :crossed_swords:**\n
+# ***remove_levels***\n**```fix\nAliases: levels_d```**\n*```Prevents level-up embeds from posting in the server.```*\n***enable_levels***\n**```fix\nAliases: levels_e```**\n*```If level-up embeds were disabled, this re-enables them.```*\n***purge***\n*```Clears inputted message count, not counting the command message.```*\n
+# **:white_heart: __GENERAL__ :white_heart:**\n
+# ***help***\n*```Legends say you've found this command already. 👀```*\n***profile***\n**```fix\nAliases: userinfo, info, stats, userstats```**\n*```Views the profile of a provided user!```*\n***level***\n**```fix\nAliases: pokémon, pokemon```**\n*```Shows the current level and experience of a provided user!```*\n
+# **:french_bread: __FUN__ :french_bread:**\n
+# ***hello***\n**```fix\nAliases: "hemlo", "hoi"```**\n*```I will greet you back!```*\n***AskDottie***\n**```fix\nAliases: ask, 8ball```**\n*```Ask me anything, I'll give a random answer!```*\n***rate***\n*```Give me anything and I'll give it a rating!```*\n
+# **:frame_photo: __IMAGE__ :frame_photo:**\n
+# ***photo***\n*```Pulls a random image of me!```*\n***nsfw_photo***\n**```css\n[NSFW CHANNEL ONLY]```**\n*```Pulls a random image of me, but be warned, they are gore.```*\n***art***\n*```Takes the most recent image in a channel and only states the truth!```*\n
+# **:headphones: __VOICE__ :headphones:**\n
+# ***connect***\n**```fix\nAliases: get_your_butt_in_here, join```**\n*```Connects me to the voice channel you're in!```*\n***disconnect***\n**```fix\nAliases: go_naughty_step, leave```**\n*```Disconnects me from the voice channel I was in!```*\n***despacito***\n**```fix\nAliases: espacito```**\n*```Plays a totally normal version of Despacito!```*\n
+# **:people_hugging: __MENTAL HEALTH__ :people_hugging:**\n
+# ```json\n"No commands yet of this category!"```\n
+#  """
+#         page2.set_author(name="🐾 Help List 🌨️", url="https://github.com/smudgedpasta/Dottie/wiki", icon_url=self.dottie.user.avatar_url_as(format="png", size=4096))
+#         page2.set_footer(text="Commands are NOT case sensitive. For a more detailed command list, view the link hidden in the \"🐾 Help List 🌨️\" title! If you find any bugs or have any enquires, be sure to let my creator, smudgedpasta, know!")
+
+#         page3 = discord.Embed(colour=discord.Colour(15277667))
+#         page3.description = """*I think I need heeelp, I'm drowning in myseeelf* 🎵\n
+# **:crossed_swords: __MODERATION__ :crossed_swords:**\n
+# ***ban***\n*```Bans a user the same way as kick.```*\n***unban***\n*```Unbans a user by typing their username and discriminator. (Example: Dottie#7157)```*\n***kick***\n*```Kicks a user from the server, either by mentioning or stating their username.```*\n
+# **:white_heart: __GENERAL__ :white_heart:**\n
+# ***source***\n**```fix\nAliases: link, invite```**\n*```Sends a link to my source code and Discord invite!```*\n***random***\n*```Takes all arguments you've provided and chooses one at random! To have a sentence, put quotes around it!```*\n***wordcount***\n**```fix\nAliases: charcount, charactercount, wc, cc```**\n*```I will ask for some text. Post it afterwards and I'll tell you the word and character count!```*\n
+# **:french_bread: __FUN__ :french_bread:**\n
+# ***matchmaking***\n**```fix\nAliases: ship, love```**\n*```Ship two people/characters of your choosing! For multiple words in one, quotes work here too!```*\n***numberguess***\n**```fix\nAliases: quiz```**\n*```A "guess-the-number" guessing game!```*\n***speak***\n**```fix\nAliases: say```**\n*```Make me say something, anything, and I'll repeat! Nobody will know it was you!```*\n
+# **:frame_photo: __IMAGE__ :frame_photo:**\n
+# ***http_cats***\n**```fix\nAliases: cats, http```**\n*```Pulls a http status code with a funny cat picture and command_related caption!```*\n***marble_fox***\n**```fix\nAliases: marble```**\n*```Sends a random image of a marble fox!```*\n***dog***\n**```fix\nAliases: og, doggo, puppo```**\n*```Sends a random image of a dog!```*\n
+# **:headphones: __VOICE__ :headphones:**\n
+# ```json\n"Whoops, no further commands yet!"```\n
+# **:people_hugging: __MENTAL HEALTH__ :people_hugging:**\n
+# ```json\n"No commands yet of this category!"```\n
+# """
+#         page3.set_author(name="🐾 Help List 🌨️", url="https://github.com/smudgedpasta/Dottie/wiki", icon_url=self.dottie.user.avatar_url_as(format="png", size=4096))
+#         page3.set_footer(text="Commands are NOT case sensitive. For a more detailed command list, view the link hidden in the \"🐾 Help List 🌨️\" title! If you find any bugs or have any enquires, be sure to let my creator, smudgedpasta, know!")
+
+#         page4 = discord.Embed(colour=discord.Colour(15277667))
+#         page4.description = f"""*I think I need heeelp, I'm drowning in myseeelf* 🎵\n
+# **:crossed_swords: __MODERATION__ :crossed_swords:**\n
+# ```json\n"Whoops, no further commands yet!"```\n
+# **:white_heart: __GENERAL__ :white_heart:**\n
+# ***loop***\n*```Repeats an inputted command a specified amount of times! Example: {PREFIX}loop 5 {PREFIX}hello```*\n***ping***\n*```Returns some technical information.```*\n***avatar***\n**```fix\nAliases: icon```**\n*```Sends an image of yours or someone else's Discord avatar!```*\n
+# **:french_bread: __FUN__ :french_bread:**\n
+# ***heart***\n*```Use this with two emojis, and I'll make them a heart!```*\n***pyramid***\n*```Tell me to build a pyramid with a height of your choosing!```*\n***faker***\n*```Think you can imitate me? I will call you out!```*\n
+# **:frame_photo: __IMAGE__ :frame_photo:**\n
+# ***fox***\n*```Sends a random image of any kind of fox!```*\n***ab***\n**```fix\nAliases: dab```**\n*```ab will spell out {PREFIX}ab with my prefix, so I'll dab!```*\n***how***\n*```How.gif, that is all.```*\n
+# **:headphones: __VOICE__ :headphones:**\n
+# ```json\n"Whoops, no further commands yet!"```\n
+# **:people_hugging: __MENTAL HEALTH__ :people_hugging:**\n
+# ```json\n"No commands yet of this category!"```\n
+# """
+#         page4.set_author(name="🐾 Help List 🌨️", url="https://github.com/smudgedpasta/Dottie/wiki", icon_url=self.dottie.user.avatar_url_as(format="png", size=4096))
+#         page4.set_footer(text="Commands are NOT case sensitive. For a more detailed command list, view the link hidden in the \"🐾 Help List 🌨️\" title! If you find any bugs or have any enquires, be sure to let my creator, smudgedpasta, know!")
+
+#         pages = [page1, page2, page3, page4]
+
+#         message = await ctx.send(embed=page1)
+
+#         await message.add_reaction("🔺")
+#         await message.add_reaction("🔻")
+
+#         def user_check(reaction, user):
+#             if reaction.message.id == message.id:
+#                 if user.id == ctx.author.id or user.id in OWNERS:
+#                     return True
+#                 if user.id != self.dottie.user.id:
+#                     guild = reaction.message.guild
+#                     if guild is not None:
+#                         member = guild.get_member(user.id)
+#                         if member is not None:
+#                             if member.guild_permissions.administrator:
+#                                 return True
+    
+#         async def page_reaction_listener(page, event_type="add"):
+#             while True:
+#                 react = await self.dottie.wait_for(f"reaction_{event_type}", check=user_check)
+#                 emoji = str(react[0])
+#                 if emoji == "🔺" and page[0] > 0:
+#                     page[0] -= 1
+#                     await message.edit(embed=pages[page[0]])
+#                 if emoji == "🔻" and page[0] < len(pages) - 1:
+#                     page[0] += 1
+#                     await message.edit(embed=pages[page[0]])
+#         page = [0]
+#         create_task(page_reaction_listener(page, "add"))
+#         create_task(page_reaction_listener(page, "remove"))
+
+
     @commands.command()
     async def help(self, ctx):
-        page1 = discord.Embed(colour=discord.Colour(15277667), timestamp=ctx.message.created_at)
-        page1.set_author(name="🐾 Help List 🌨️", url="https://github.com/smudgedpasta/Dottie/wiki", icon_url=self.dottie.user.avatar_url_as(format="png", size=4096))
-        page1.description = "*I think I need heeelp, I'm drowning in myseeelf* 🎵"
-        page1.set_image(url="https://cdn.discordapp.com/attachments/683233571405561876/746281046231875594/image0.png")
-        page1.set_footer(icon_url=ctx.author.avatar_url_as(format="png", size=4096), text="Click the reactions to scroll through the pages!")
-
-        page2 = discord.Embed(colour=discord.Colour(15277667))
-        page2.description = """*I think I need heeelp, I'm drowning in myseeelf* 🎵\n
-**:crossed_swords: __MODERATION__ :crossed_swords:**\n
-***remove_levels***\n**```fix\nAliases: levels_d```**\n*```Prevents level-up embeds from posting in the server.```*\n***enable_levels***\n**```fix\nAliases: levels_e```**\n*```If level-up embeds were disabled, this re-enables them.```*\n***purge***\n*```Clears inputted message count, not counting the command message.```*\n
-**:white_heart: __GENERAL__ :white_heart:**\n
-***help***\n*```Legends say you've found this command already. 👀```*\n***profile***\n**```fix\nAliases: userinfo, info, stats, userstats```**\n*```Views the profile of a provided user!```*\n***level***\n**```fix\nAliases: pokémon, pokemon```**\n*```Shows the current level and experience of a provided user!```*\n
-**:french_bread: __FUN__ :french_bread:**\n
-***hello***\n**```fix\nAliases: "hemlo", "hoi"```**\n*```I will greet you back!```*\n***AskDottie***\n**```fix\nAliases: ask, 8ball```**\n*```Ask me anything, I'll give a random answer!```*\n***rate***\n*```Give me anything and I'll give it a rating!```*\n
-**:frame_photo: __IMAGE__ :frame_photo:**\n
-***photo***\n*```Pulls a random image of me!```*\n***nsfw_photo***\n**```css\n[NSFW CHANNEL ONLY]```**\n*```Pulls a random image of me, but be warned, they are gore.```*\n***art***\n*```Takes the most recent image in a channel and only states the truth!```*\n
-**:headphones: __VOICE__ :headphones:**\n
-***connect***\n**```fix\nAliases: get_your_butt_in_here, join```**\n*```Connects me to the voice channel you're in!```*\n***disconnect***\n**```fix\nAliases: go_naughty_step, leave```**\n*```Disconnects me from the voice channel I was in!```*\n***despacito***\n**```fix\nAliases: espacito```**\n*```Plays a totally normal version of Despacito!```*\n
-**:people_hugging: __MENTAL HEALTH__ :people_hugging:**\n
-```json\n"No commands yet of this category!"```\n
- """
-        page2.set_author(name="🐾 Help List 🌨️", url="https://github.com/smudgedpasta/Dottie/wiki", icon_url=self.dottie.user.avatar_url_as(format="png", size=4096))
-        page2.set_footer(text="Commands are NOT case sensitive. For a more detailed command list, view the link hidden in the \"🐾 Help List 🌨️\" title! If you find any bugs or have any enquires, be sure to let my creator, smudgedpasta, know!")
-
-        page3 = discord.Embed(colour=discord.Colour(15277667))
-        page3.description = """*I think I need heeelp, I'm drowning in myseeelf* 🎵\n
-**:crossed_swords: __MODERATION__ :crossed_swords:**\n
-***ban***\n*```Bans a user the same way as kick.```*\n***unban***\n*```Unbans a user by typing their username and discriminator. (Example: Dottie#7157)```*\n***kick***\n*```Kicks a user from the server, either by mentioning or stating their username.```*\n
-**:white_heart: __GENERAL__ :white_heart:**\n
-***source***\n**```fix\nAliases: link, invite```**\n*```Sends a link to my source code and Discord invite!```*\n***random***\n*```Takes all arguments you've provided and chooses one at random! To have a sentence, put quotes around it!```*\n***wordcount***\n**```fix\nAliases: charcount, charactercount, wc, cc```**\n*```I will ask for some text. Post it afterwards and I'll tell you the word and character count!```*\n
-**:french_bread: __FUN__ :french_bread:**\n
-***matchmaking***\n**```fix\nAliases: ship, love```**\n*```Ship two people/characters of your choosing! For multiple words in one, quotes work here too!```*\n***numberguess***\n**```fix\nAliases: quiz```**\n*```A "guess-the-number" guessing game!```*\n***speak***\n**```fix\nAliases: say```**\n*```Make me say something, anything, and I'll repeat! Nobody will know it was you!```*\n
-**:frame_photo: __IMAGE__ :frame_photo:**\n
-***http_cats***\n**```fix\nAliases: cats, http```**\n*```Pulls a http status code with a funny cat picture and command_related caption!```*\n***marble_fox***\n**```fix\nAliases: marble```**\n*```Sends a random image of a marble fox!```*\n***dog***\n**```fix\nAliases: og, doggo, puppo```**\n*```Sends a random image of a dog!```*\n
-**:headphones: __VOICE__ :headphones:**\n
-```json\n"Whoops, no further commands yet!"```\n
-**:people_hugging: __MENTAL HEALTH__ :people_hugging:**\n
-```json\n"No commands yet of this category!"```\n
-"""
-        page3.set_author(name="🐾 Help List 🌨️", url="https://github.com/smudgedpasta/Dottie/wiki", icon_url=self.dottie.user.avatar_url_as(format="png", size=4096))
-        page3.set_footer(text="Commands are NOT case sensitive. For a more detailed command list, view the link hidden in the \"🐾 Help List 🌨️\" title! If you find any bugs or have any enquires, be sure to let my creator, smudgedpasta, know!")
-
-        page4 = discord.Embed(colour=discord.Colour(15277667))
-        page4.description = f"""*I think I need heeelp, I'm drowning in myseeelf* 🎵\n
-**:crossed_swords: __MODERATION__ :crossed_swords:**\n
-```json\n"Whoops, no further commands yet!"```\n
-**:white_heart: __GENERAL__ :white_heart:**\n
-***loop***\n*```Repeats an inputted command a specified amount of times! Example: {PREFIX}loop 5 {PREFIX}hello```*\n***ping***\n*```Returns some technical information.```*\n***avatar***\n**```fix\nAliases: icon```**\n*```Sends an image of yours or someone else's Discord avatar!```*\n
-**:french_bread: __FUN__ :french_bread:**\n
-***heart***\n*```Use this with two emojis, and I'll make them a heart!```*\n***pyramid***\n*```Tell me to build a pyramid with a height of your choosing!```*\n***faker***\n*```Think you can imitate me? I will call you out!```*\n
-**:frame_photo: __IMAGE__ :frame_photo:**\n
-***fox***\n*```Sends a random image of any kind of fox!```*\n***ab***\n**```fix\nAliases: dab```**\n*```ab will spell out {PREFIX}ab with my prefix, so I'll dab!```*\n***how***\n*```How.gif, that is all.```*\n
-**:headphones: __VOICE__ :headphones:**\n
-```json\n"Whoops, no further commands yet!"```\n
-**:people_hugging: __MENTAL HEALTH__ :people_hugging:**\n
-```json\n"No commands yet of this category!"```\n
-"""
-        page4.set_author(name="🐾 Help List 🌨️", url="https://github.com/smudgedpasta/Dottie/wiki", icon_url=self.dottie.user.avatar_url_as(format="png", size=4096))
-        page4.set_footer(text="Commands are NOT case sensitive. For a more detailed command list, view the link hidden in the \"🐾 Help List 🌨️\" title! If you find any bugs or have any enquires, be sure to let my creator, smudgedpasta, know!")
-
-        pages = [page1, page2, page3, page4]
-
-        message = await ctx.send(embed=page1)
-
-        await message.add_reaction("🔺")
-        await message.add_reaction("🔻")
-
-        def user_check(reaction, user):
-            if reaction.message.id == message.id:
-                if user.id == ctx.author.id or user.id in OWNERS:
-                    return True
-                if user.id != self.dottie.user.id:
-                    guild = reaction.message.guild
-                    if guild is not None:
-                        member = guild.get_member(user.id)
-                        if member is not None:
-                            if member.guild_permissions.administrator:
-                                return True
-    
-        async def page_reaction_listener(page, event_type="add"):
-            while True:
-                react = await self.dottie.wait_for(f"reaction_{event_type}", check=user_check)
-                emoji = str(react[0])
-                if emoji == "🔺" and page[0] > 0:
-                    page[0] -= 1
-                    await message.edit(embed=pages[page[0]])
-                if emoji == "🔻" and page[0] < len(pages) - 1:
-                    page[0] += 1
-                    await message.edit(embed=pages[page[0]])
-        page = [0]
-        create_task(page_reaction_listener(page, "add"))
-        create_task(page_reaction_listener(page, "remove"))
-
-
-    @commands.command()
-    async def newhelp(self, ctx):
         home = discord.Embed(colour=discord.Colour(15277667), timestamp=ctx.message.created_at)
         home.set_author(name="🐾 Help List 🌨️", url="https://github.com/smudgedpasta/Dottie/wiki", icon_url=self.dottie.user.avatar_url_as(format="png", size=4096))
         home.description = "*I think I need heeelp, I'm drowning in myseeelf* 🎵"
