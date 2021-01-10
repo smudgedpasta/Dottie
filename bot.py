5b97e3818f52ff66baa8c3865eefa7cdd265fd74
smudgedpasta <smudgedpasta@gmail.com>
2021-01-10 17:18:00 +0000

    Apparently this runs Python twice?

--- bot.py
+++ bot.py
@@ -390,7 +390,7 @@ async def log_remove(ctx):
 async def load(ctx, extension=None):
     if extension is None:
 
-        start_miza()
+        # start_miza()
         for cog in ["moderation", "general", "fun", "levels", "image", "voice", "owner", "CTE"]:
             dottie.load_extension(f"cogs.{cog}")
         embed = discord.Embed(colour=discord.Colour(255))
@@ -399,8 +399,8 @@ async def load(ctx, extension=None):
         return
 
     dottie.load_extension(f"cogs.{extension}")
-    if extension == "voice":
-        start_miza()
+    # if extension == "voice":
+        # start_miza()
     embed = discord.Embed(colour=discord.Colour(255))
     embed.description = f"```ini\n[Successfully returned access to category \"{extension.upper()}\".]```"
     await ctx.send(embed=embed)
@@ -411,7 +411,7 @@ async def load(ctx, extension=None):
 async def unload(ctx, extension=None):
     if extension is None:
 
-        stop_miza()
+        # stop_miza()
         for cog in ["moderation", "general", "fun", "levels", "image", "voice", "owner", "CTE"]:
             dottie.unload_extension(f"cogs.{cog}")
         embed = discord.Embed(colour=discord.Colour(16711680))
@@ -420,8 +420,8 @@ async def unload(ctx, extension=None):
         return
 
     dottie.unload_extension(f"cogs.{extension}")
-    if extension == "voice":
-        stop_miza()
+    # if extension == "voice":
+    #     stop_miza()
     embed = discord.Embed(colour=discord.Colour(16711680))
     embed.description = f"```asciidoc\n[Successfully removed category \"{extension.upper()}\" until further notice.]```"
     await ctx.send(embed=embed)
@@ -432,8 +432,8 @@ async def unload(ctx, extension=None):
 async def reload(ctx, extension=None):
     if extension is None:
         
-        stop_miza()
-        start_miza()
+        # stop_miza()
+        # start_miza()
         for cog in ["moderation", "general", "fun", "levels", "image", "voice", "owner", "CTE"]:
             dottie.unload_extension(f"cogs.{cog}")
             dottie.load_extension(f"cogs.{cog}")
@@ -444,9 +444,9 @@ async def reload(ctx, extension=None):
 
     dottie.unload_extension(f"cogs.{extension}")
     dottie.load_extension(f"cogs.{extension}")
-    if extension == "voice":
-        stop_miza()
-        start_miza()
+    # if extension == "voice":
+    #     stop_miza()
+    #     start_miza()
     embed = discord.Embed(colour=discord.Colour(16776960))
     embed.description = f"```fix\n[Successfully refreshed category \"{extension.upper()}\".]```"
     await ctx.send(embed=embed)
