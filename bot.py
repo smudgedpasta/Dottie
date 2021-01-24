d346f8f6487b429e29bb6a92f4bb259374e5c769
smudgedpasta <smudgedpasta@gmail.com>
2021-01-24 20:56:47 +0000

    Yeah I give up lol

--- bot.py
+++ bot.py
@@ -160,15 +160,23 @@ async def on_message(message):
 
     await dottie.invoke(ctx)
 
-    MESSAGE = message.content
-    def split(MESSAGE, *args):
-            return [char for char in MESSAGE]
-    MESSAGE = message.content.split()
-    if MESSAGE == "#":
+    # MESSAGE = message.content
+    # def split(MESSAGE, *args):
+    #         return [char for char in MESSAGE]
+    # MESSAGE = message.content.split()
+    # if MESSAGE == "#":
+    #         return
+    # else:
+    #     Smudge = [530781444742578188, 668064931345596439]
+    #     if message.author.id in Smudge and message.content.endswith("#"):
+    #         await ctx.send("Smudge Keyboard Moment <a:moment:750685242553139321>")
+
+    Smudge = [530781444742578188, 668064931345596439]
+    if message.author.id in Smudge and message.content.endswith("#"):
+        MESSAGE = list(message.content)
+        if MESSAGE == "#":
             return
-    else:
-        Smudge = [530781444742578188, 668064931345596439]
-        if message.author.id in Smudge and message.content.endswith("#"):
+        else:
             await ctx.send("Smudge Keyboard Moment <a:moment:750685242553139321>")
 
     if ctx.command is not None:
